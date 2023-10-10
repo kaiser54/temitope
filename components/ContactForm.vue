909ef4ded59a104b59ad9e5a6011b43e91429ef2
Agboola Temitope <85450650+kaiser54@users.noreply.github.com>
2023-10-10 16:47:30 +0100

    error message

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,29 +1,72 @@
 <template>
   <form ref="form" @submit.prevent="sendEmail">
     <div class="name-input">
-      <div class="field">
+      <div class="field" :class="{ 'has-error': nameError }">
         <label class="sans">Name</label>
-        <input required class="input" type="text" name="user_name" />
+        <input
+          required
+          class="input"
+          type="text"
+          name="user_name"
+          v-model="name"
+        />
+        <span class="error-message" v-if="nameError">
+          <span class="material-symbols-sharp"> error </span>
+          {{ nameError }}
+        </span>
       </div>
-      <div class="field">
+      <div class="field" :class="{ 'has-error': emailError }">
         <label class="sans">Email</label>
-        <input required class="input" type="email" name="user_email" />
+        <input
+          required
+          class="input"
+          type="email"
+          name="user_email"
+          v-model="email"
+        />
+        <span class="error-message" v-if="emailError">
+          <span class="material-symbols-sharp"> error </span>
+          {{ emailError }}
+        </span>
       </div>
     </div>
-    <div class="field">
+    <div class="field" :class="{ 'has-error': messageError }">
       <label class="sans">Message</label>
-      <textarea class="input" name="message"></textarea>
+      <textarea class="input" name="message" v-model="message"></textarea>
+      <span class="error-message" v-if="messageError">
+        <span class="material-symbols-sharp"> error </span>
+        {{ messageError }}
+      </span>
     </div>
     <input required class="btn" type="submit" value="SHOOOOOT" />
   </form>
 </template>
-  
-  <script>
+
+<script>
 import emailjs from "@emailjs/browser";
 
 export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+      nameError: "",
+      emailError: "",
+      messageError: "",
+    };
+  },
   methods: {
     sendEmail() {
+      // Validation
+      this.nameError = !this.name ? "Name is required." : "";
+      this.emailError = !this.email ? "Email is required." : "";
+      this.messageError = !this.message ? "Message is required." : "";
+
+      if (!this.name || !this.email || !this.message) {
+        return; // Stop submission if there are errors
+      }
+
       emailjs
         .sendForm(
           "service_r1w6gfe",
@@ -41,45 +84,66 @@ export default {
         );
     },
   },
+  watch: {
+    name() {
+      this.nameError = "";
+    },
+    email() {
+      this.emailError = "";
+    },
+    message() {
+      this.messageError = "";
+    },
+  },
 };
 </script>
 
+
 <style lang="sass" scoped>
 form
-    display: flex
-    flex-direction: column
-    gap: 32px
-    width: 100%
+  display: flex
+  flex-direction: column
+  gap: 32px
+  width: 100%
 .name-input
-    display: flex
-    gap: 32px
-    width: 100%
+  display: flex
+  gap: 32px
+  width: 100%
 .sans
-    font-family: "DM Sans"
+  font-family: "DM Sans"
 
 label
-    color: white
+  color: white
 textarea
-    padding: 10px
-    width: 100%
-    height: 150px
-    resize: none
+  padding: 10px
+  width: 100%
+  height: 150px
+  resize: none
 .btn
-    display: flex
-    flex-direction: row
-    justify-content: center
-    align-items: center
+  display: flex
+  flex-direction: row
+  justify-content: center
+  align-items: center
 
-    border-radius: 10px
+  border-radius: 10px
 
-    width: 100%
-    padding: 10px
+  width: 100%
+  padding: 10px
+  color: black !important
+  outline: 0
+  border: 0
 
-    outline: 0
-    border: 0
+  cursor: pointer
+  overflow: hidden
 
-    cursor: pointer
-    overflow: hidden
+  transition: 0.3s
 
-    transition: 0.3s
+.error-message,
+.material-symbols-sharp
+  display: flex
+  align-items: center
+  color: red
+  font-weight: 100
+input
+  color: white
 </style>
